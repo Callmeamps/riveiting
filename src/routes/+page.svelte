68001ad4c096c6a7d26b90da27f7887e3443dbab
Callmeamps <95033065+Callmeamps@users.noreply.github.com>
2023-06-11 20:08:56 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+import onMount from "svelte";
+import rive from "@rive-app/canvas";
+
+
+onMount(() => {
+	const rivCanvas = document.getElementById("rive");
+	let mainCard = document.getElementByClass("card");
+	let r = new rive({
+		src: "./reach.riv",
+		autoplay: true,
+		artboard: "reach",
+		stateMachine: "PullDown",
+		canvas: rivCanvas,
+		onLoad: () => {
+			r.resizeDrawingSurfaceToCanvas();
+		},
+	});
+});
+</script>
+<div class="card">
+	<canvas id="rive"></canvas>
+</div>
